5dde4888d6edc317b2d6585f8626bb7b15d7e076
“musikisomorphie” <wujiqing9@gmail.com>
2022-06-06 12:04:33 +0200

    ready for single channel training on dgx

--- run_rxrx1.sh
+++ run_rxrx1.sh
@@ -1,7 +1,7 @@
 python -m torch.distributed.launch --nproc_per_node=1 --master_port=12341 train.py \
-    /root/Data/non_IID/decoder/rxrx19b_cell_lmdb/
-    --batch 8 
-    --iter 800000 
-    --size 128    
-    --channel 1
-    --check_save /root/Data/non_IID/encoder/rxrx19b_cell/chn1/  
+    /root/Data/non_IID/decoder/rxrx19b_cell_lmdb/ \
+    --batch=8 \
+    --iter=800000 \
+    --size=128 \
+    --channel=1 \
+    --check_save=/root/Data/non_IID/encoder/rxrx19b_chn1/
