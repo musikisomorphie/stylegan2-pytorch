4ae6d38c1bcf99ea9383cf69432a2d8add1a54c3
“musikisomorphie” <wujiqing9@gmail.com>
2022-08-10 12:30:31 +0200

    ready for retraining 19b

---
+++ run_gpu0b.sh
@@ -0,0 +1,15 @@
+python -m torch.distributed.launch --nproc_per_node=1 --master_port=12340 train.py \
+    /root/Data/non_IID/decoder/rxrx19b_HUVEC_lmdb/ \
+    --batch=8 \
+    --iter=800000 \
+    --size=128 \
+    --channel=0 \
+    --check_save=/root/Data/non_IID/encoder/rxrx19b_HUVEC_chn0/
+
+python -m torch.distributed.launch --nproc_per_node=1 --master_port=12340 train.py \
+    /root/Data/non_IID/decoder/rxrx19b_HUVEC_lmdb/ \
+    --batch=8 \
+    --iter=800000 \
+    --size=128 \
+    --channel=1 \
+    --check_save=/root/Data/non_IID/encoder/rxrx19b_HUVEC_chn1/
