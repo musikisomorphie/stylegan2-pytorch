d6e82fa60b82b81eaf64310cda6b9d298fb95320
“musikisomorphie” <wujiqing9@gmail.com>
2022-08-19 08:54:22 +0200

    split gpu jobs

--- run_gpu4b.sh
+++ run_gpu4b.sh
@@ -3,5 +3,5 @@ python -m torch.distributed.launch --nproc_per_node=1 --master_port=12340 train.
     --batch=8 \
     --iter=800000 \
     --size=128 \
-    --channel=4 \
-    --check_save=/root/Data/non_IID/encoder/rxrx19b_HUVEC_chn4/
+    --channel=5 \
+    --check_save=/root/Data/non_IID/encoder/rxrx19b_HUVEC_chn5/
