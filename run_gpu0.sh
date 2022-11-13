83c4cbbb44480ee032b52cd7c635774e06aee68e
musikisomorphie <wujiqing9@gmail.com>
2022-11-13 20:02:09 +0100

    Update run_gpu0.sh

--- run_gpu0.sh
+++ run_gpu0.sh
@@ -1,15 +1,15 @@
-CUDA_VISIBLE_DEVICES=0 python -m train.py \
-    Data/non_IID/decoder/rxrx19b_HUVEC_lmdb/ \
+CUDA_VISIBLE_DEVICES=0 python train.py \
+    /raid/jiqing/Data/non_IID/decoder/rxrx19b_HUVEC_lmdb/ \
     --batch=8 \
     --iter=800000 \
     --size=128 \
     --channel=0 \
-    --check_save=/root/Data/non_IID/encoder/rxrx19b_HUVEC_chn0/
+    --check_save=/raid/jiqing/Data/non_IID/encoder/rxrx19b_HUVEC_chn0/
 
-CUDA_VISIBLE_DEVICES=0 python -m train.py \
-    Data/non_IID/decoder/rxrx19b_HUVEC_lmdb/ \
+CUDA_VISIBLE_DEVICES=0 python train.py \
+    /raid/jiqing/Data/non_IID/decoder/rxrx19b_HUVEC_lmdb/ \
     --batch=8 \
     --iter=800000 \
     --size=128 \
     --channel=1 \
-    --check_save=/root/Data/non_IID/encoder/rxrx19b_HUVEC_chn1/
+    --check_save=/raid/jiqing/Data/non_IID/encoder/rxrx19b_HUVEC_chn1/
