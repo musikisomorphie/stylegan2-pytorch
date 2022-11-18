13edd53c047ee33683749095fa94143a117d0ed2
“musikisomorphie” <wujiqing9@gmail.com>
2022-11-18 07:51:53 +0100

    retrain chn2

--- run_gpu1.sh
+++ run_gpu1.sh
@@ -1,15 +1,16 @@
 CUDA_VISIBLE_DEVICES=1 python train.py \
     /raid/jiqing/Data/non_IID/decoder/rxrx19b_HUVEC_lmdb/ \
     --batch=8 \
-    --iter=800000 \
+    --iter=1600000 \
     --size=128 \
     --channel=2 \
-    --check_save=/raid/jiqing/Data/non_IID/encoder/rxrx19b_HUVEC_chn2/
+    --check_save=/raid/jiqing/Data/non_IID/encoder/rxrx19b_HUVEC_chn2/ \
+    --ckpt=/raid/jiqing/Data/non_IID/encoder/rxrx19b_HUVEC_chn2/790000.pt
 
-CUDA_VISIBLE_DEVICES=1 python train.py \
-    /raid/jiqing/Data/non_IID/decoder/rxrx19b_HUVEC_lmdb/ \
-    --batch=8 \
-    --iter=800000 \
-    --size=128 \
-    --channel=3 \
-    --check_save=/raid/jiqing/Data/non_IID/encoder/rxrx19b_HUVEC_chn3/
+# CUDA_VISIBLE_DEVICES=1 python train.py \
+#     /raid/jiqing/Data/non_IID/decoder/rxrx19b_HUVEC_lmdb/ \
+#     --batch=8 \
+#     --iter=800000 \
+#     --size=128 \
+#     --channel=3 \
+#     --check_save=/raid/jiqing/Data/non_IID/encoder/rxrx19b_HUVEC_chn3/
--- run_gpu2.sh
+++ run_gpu2.sh
@@ -1,15 +1,23 @@
 CUDA_VISIBLE_DEVICES=2 python train.py \
     /raid/jiqing/Data/non_IID/decoder/rxrx19b_HUVEC_lmdb/ \
-    --batch=8 \
+    --batch=16 \
     --iter=800000 \
     --size=128 \
-    --channel=4 \
-    --check_save=/raid/jiqing/Data/non_IID/encoder/rxrx19b_HUVEC_chn4/
+    --channel=2 \
+    --check_save=/raid/jiqing/Data/non_IID/encoder/rxrx19b_HUVEC_chn2_16/
 
-CUDA_VISIBLE_DEVICES=2 python train.py \
-    /raid/jiqing/Data/non_IID/decoder/rxrx19b_HUVEC_lmdb/ \
-    --batch=8 \
-    --iter=800000 \
-    --size=128 \
-    --channel=5 \
-    --check_save=/raid/jiqing/Data/non_IID/encoder/rxrx19b_HUVEC_chn5/
+# CUDA_VISIBLE_DEVICES=2 python train.py \
+#     /raid/jiqing/Data/non_IID/decoder/rxrx19b_HUVEC_lmdb/ \
+#     --batch=8 \
+#     --iter=800000 \
+#     --size=128 \
+#     --channel=4 \
+#     --check_save=/raid/jiqing/Data/non_IID/encoder/rxrx19b_HUVEC_chn4/
+
+# CUDA_VISIBLE_DEVICES=2 python train.py \
+#     /raid/jiqing/Data/non_IID/decoder/rxrx19b_HUVEC_lmdb/ \
+#     --batch=8 \
+#     --iter=800000 \
+#     --size=128 \
+#     --channel=5 \
+#     --check_save=/raid/jiqing/Data/non_IID/encoder/rxrx19b_HUVEC_chn5/
