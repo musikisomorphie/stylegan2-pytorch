cf7db4de499ee1ed96447b789190e609ee668c5a
musikisomorphie <wujiqing9@gmail.com>
2022-11-20 09:29:53 +0100

    Update run_gpu1.sh

--- run_gpu1.sh
+++ run_gpu1.sh
@@ -5,7 +5,7 @@ CUDA_VISIBLE_DEVICES=1 python train.py \
     --size=128 \
     --channel=2 \
     --check_save=/raid/jiqing/Data/non_IID/encoder/rxrx19b_HUVEC_chn2/ \
-    --ckpt=/raid/jiqing/Data/non_IID/encoder/rxrx19b_HUVEC_chn2/790000.pt
+    --ckpt=/raid/jiqing/Data/non_IID/encoder/rxrx19b_HUVEC_chn2/checkpoint/790000.pt
 
 # CUDA_VISIBLE_DEVICES=1 python train.py \
 #     /raid/jiqing/Data/non_IID/decoder/rxrx19b_HUVEC_lmdb/ \
