1e4654d96870d1bf22abf2adabe9a9457113dacf
“musikisomorphie” <wujiqing9@gmail.com>
2022-11-24 08:16:16 +0100

    finetune chn2

--- run_gpu1.sh
+++ run_gpu1.sh
@@ -1,11 +1,12 @@
 CUDA_VISIBLE_DEVICES=1 python train.py \
     /raid/jiqing/Data/non_IID/decoder/rxrx19b_HUVEC_lmdb/ \
     --batch=8 \
-    --iter=1600000 \
+    --iter=1000000 \
     --size=128 \
     --channel=2 \
-    --check_save=/raid/jiqing/Data/non_IID/encoder/rxrx19b_HUVEC_chn2/ \
-    --ckpt=/raid/jiqing/Data/non_IID/encoder/rxrx19b_HUVEC_chn2/790000.pt
+    --lr=0.001 \
+    --check_save=/raid/jiqing/Data/non_IID/encoder/rxrx19b_HUVEC_chn2_128/ \
+    --ckpt=/raid/jiqing/Data/non_IID/encoder/rxrx19b_HUVEC_chn2_64/checkpoint/790000.pt
 
 # CUDA_VISIBLE_DEVICES=1 python train.py \
 #     /raid/jiqing/Data/non_IID/decoder/rxrx19b_HUVEC_lmdb/ \
