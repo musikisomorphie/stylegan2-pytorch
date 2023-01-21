a292262f66ce85f830f9caa631b59a6f6998bdac
“musikisomorphie” <wujiqing9@gmail.com>
2023-01-21 19:04:21 +0100

    minor change

--- run_nstg1.sh
+++ run_nstg1.sh
@@ -1,15 +1,7 @@
 CUDA_VISIBLE_DEVICES=1 python train.py \
     /raid/jiqing/Data/NSTG/GAN/ \
-    --batch=16 \
+    --batch=8 \
     --iter=400000 \
     --size=128 \
     --channel=-1 \
-    --check_save=/raid/jiqing/Data/NSTG/GAN/decoder2/
-
-CUDA_VISIBLE_DEVICES=1 python train.py \
-    /raid/jiqing/Data/NSTG/GAN/ \
-    --batch=16 \
-    --iter=400000 \
-    --size=128 \
-    --channel=-1 \
-    --check_save=/raid/jiqing/Data/NSTG/GAN/decoder3/
+    --check_save=/raid/jiqing/Data/NSTG/GAN/decoder1/
--- run_nstg3.sh
+++ run_nstg3.sh
@@ -1,7 +1,7 @@
-CUDA_VISIBLE_DEVICES=0 python train.py \
+CUDA_VISIBLE_DEVICES=3 python train.py \
     /raid/jiqing/Data/NSTG/GAN/ \
     --batch=8 \
     --iter=400000 \
     --size=128 \
     --channel=-1 \
-    --check_save=/raid/jiqing/Data/NSTG/GAN/decoder0/
+    --check_save=/raid/jiqing/Data/NSTG/GAN/decoder3/
