48844df5add8463b394b40ab9fed1116b8cd9969
“musikisomorphie” <wujiqing9@gmail.com>
2023-02-03 09:34:17 +0100

    convert NSTG to CosMx

--- run_cosmx1.sh
+++ run_cosmx1.sh
@@ -1,15 +1,15 @@
 CUDA_VISIBLE_DEVICES=1 python train.py \
-    /raid/jiqing/Data/NSTG/GAN/ \
+    /raid/jiqing/Data/CosMx/GAN/ \
     --batch=8 \
     --iter=400000 \
     --size=128 \
     --channel=-1 \
-    --check_save=/raid/jiqing/Data/NSTG/GAN/decoder2/
+    --check_save=/raid/jiqing/Data/CosMx/GAN/decoder2/
 
 CUDA_VISIBLE_DEVICES=1 python train.py \
-    /raid/jiqing/Data/NSTG/GAN/ \
+    /raid/jiqing/Data/CosMx/GAN/ \
     --batch=8 \
     --iter=400000 \
     --size=128 \
     --channel=-1 \
-    --check_save=/raid/jiqing/Data/NSTG/GAN/decoder3/
+    --check_save=/raid/jiqing/Data/CosMx/GAN/decoder3/
